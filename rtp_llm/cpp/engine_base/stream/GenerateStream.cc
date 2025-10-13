3758c3d280ceeac761f2aacd46b3489692c4148c
zhangjianning.zjn <zhangjianning.zjn@alibaba-inc.com>
2025-10-13 14:07:53 +0800

    fix: remove redundent prefill when n > 1 and no beam search

--- rtp_llm/cpp/engine_base/stream/GenerateStream.cc
+++ rtp_llm/cpp/engine_base/stream/GenerateStream.cc
@@ -85,8 +85,9 @@ GenerateStream::GenerateStream(const shared_ptr<GenerateInput>& input,
 
     setReturnAllProbs(generate_input_->generate_config->return_all_probs);
 
-    think_logits_processor_ptr_ = ThinkModeLogitsProcessor::fromGenerateInput(device_, generate_input_, maxBatchSize());
-    tree_logits_processor_ptr_  = TreeLogitsProcessor::fromGenerateInput(device_, generate_input_, init_batch_size);
+    think_logits_processor_ptr_ =
+        ThinkModeLogitsProcessor::fromGenerateInput(device_, generate_input_, init_batch_size);
+    tree_logits_processor_ptr_ = TreeLogitsProcessor::fromGenerateInput(device_, generate_input_, init_batch_size);
     beam_search_logits_processor_ptr_ =
         BeamSearchLogitsProcessor::fromGenerateInput(device_, generate_input_, special_tokens_.eos_token_id_);
 
@@ -226,7 +227,7 @@ int GenerateStream::batchSize(int output_len) const {
     if (generate_input_->generate_config->hasNumBeams()) {
         return numBeams(output_len);
     } else {
-        return std::max(numReturnSequences(), 1);
+        return output_len == 0 && !perf_test_ ? 1 : std::max(numReturnSequences(), 1);
     }
 }
 
@@ -868,7 +869,7 @@ bool GenerateStream::updateKvCacheBlocks(const rtp_llm::BufferPtr& src_batch_ind
 }
 
 void GenerateStream::beamSearchLogitProcessorUpdate(const rtp_llm::BufferPtr& beam_idx) {
-    if (beam_idx == nullptr || !hasNumBeams()) {
+    if (beam_idx == nullptr) {
         return;
     }
 
