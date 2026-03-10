a7319d501f8baea82c9ae8e1195c3eb0f217606e
zhangjianning.zjn <zhangjianning.zjn@alibaba-inc.com>
2026-03-10 17:38:55 +0800

    feat: support shallow copy for PyAttentionInputs

--- rtp_llm/models_py/bindings/OpDefs.cc
+++ rtp_llm/models_py/bindings/OpDefs.cc
@@ -92,8 +92,9 @@ void registerPyOpDefs(pybind11::module& m) {
         .def_readwrite("cache_store_inputs", &PyAttentionInputs::cache_store_inputs)
         .def_readwrite("context_parallel_info", &PyAttentionInputs::context_parallel_info)
         .def_readwrite("combo_position_ids", &PyAttentionInputs::combo_position_ids)
+        .def_readwrite("prefill_cuda_graph_copy_params", &PyAttentionInputs::prefill_cuda_graph_copy_params)
         .def("__repr__", [](const PyAttentionInputs& self) { return "PyAttentionInputs"; })
-        .def_readwrite("prefill_cuda_graph_copy_params", &PyAttentionInputs::prefill_cuda_graph_copy_params);
+        .def("__copy__", [](const PyAttentionInputs& self) { return PyAttentionInputs(self); });
 
     pybind11::class_<BertEmbeddingInputs>(m, "BertEmbeddingInputs")
         .def(pybind11::init<>())
